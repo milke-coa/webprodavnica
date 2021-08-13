0cbab72e91a24e581281324418e16aaf2d8d63c3
Aleksandar Milic <aleksandar.milic.1904@gmail.com>
2021-08-13 21:23:37 +0200

    Inicial commit

---
+++ src/app.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller()
+export class AppController {
+  @Get()
+  getHello(): string {
+    return 'Hello world';
+  }
+  @Get('world')
+  getWorld(): string {
+    return 'World';
+  }
+}
